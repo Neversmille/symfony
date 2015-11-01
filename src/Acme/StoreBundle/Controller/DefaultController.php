1c6302775819e45abf0bb43907404afc389b4e95
Smile <foreverneversmile@gmail.com>
2015-11-01 17:50:57 +0200

    Learn Doctrine

---
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Acme\StoreBundle\Controller;
+
+use Acme\StoreBundle\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * Стандартный контроллер
+     *
+     * @param $name
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction($name)
+    {
+        return $this->render('AcmeStoreBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * Добавление данных
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createAction() {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription("Lorem ipsum");
+
+        //Менеджер ответственный за управление процессами сохранения и получения обьектов БД
+        $em = $this->getDoctrine()->getManager();
+        //Сообщает команду на "управление обьектом" $product
+        $em->persist($product);
+        //Просматривает все обьекты которыми управляет, чтобы узнать, надо ли сохранять их в БД и выполняет запросы
+        $em->flush();
+        return $this->render('AcmeStoreBundle:Default:insert.html.twig', array('id' => $product->getId()));
+    }
+
+    /**
+     * Вывод всех данных из таблицы
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function allAction() {
+        $repository = $this->getDoctrine()
+            ->getRepository('AcmeStoreBundle:Product');
+        $products = $repository->findAll();
+        return $this->render('AcmeStoreBundle:Default:all.html.twig', array('products' => $products));
+
+    }
+}
---
+++ src/Acme/StoreBundle/Entity/Product.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smile
+ * Date: 01.11.15
+ * Time: 11:24
+ */
+
+namespace Acme\StoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Product
+ */
+class Product {
+    protected $id;
+    protected $name;
+    protected $price;
+    protected $description;
+
+    /**
+     * Get id
+     *
+     * @return \intefer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
